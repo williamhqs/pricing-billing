06f2a14b93f02e1568a5edf2f39eac77b94f2b77
qiushi <william.hqs@gmail.com>
2026-08-31 16:57:14 +0800

    Regenerate by Prism fixing the fgrid auto layout issue.

--- src/component/PayeeInformation.tsx
+++ src/component/PayeeInformation.tsx
@@ -5,13 +5,19 @@ export default function PayeeInformation() {
         display: "flex",
         flexDirection: "column",
         gap: 0,
-        paddingTop: 16,
+        paddingTop: 0,
         paddingRight: 0,
         paddingBottom: 0,
         paddingLeft: 0,
         justifyContent: "flex-start",
         alignItems: "flex-start",
         width: "100%",
+        height: 210,
+        backgroundColor: "#ffffff",
+        borderRadius: 6,
+        border: "1px solid #e5e7eb",
+        boxShadow: "0px 1px 2px #1a2a4a0a",
+        overflow: "hidden",
       }}
     >
       <div
@@ -19,69 +25,140 @@ export default function PayeeInformation() {
           display: "flex",
           flexDirection: "column",
           gap: 0,
-          paddingTop: 0,
-          paddingRight: 0,
-          paddingBottom: 0,
-          paddingLeft: 0,
+          paddingTop: 12,
+          paddingRight: 20,
+          paddingBottom: 12,
+          paddingLeft: 20,
           justifyContent: "flex-start",
           alignItems: "flex-start",
           width: "100%",
-          height: 210,
-          backgroundColor: "#ffffff",
-          borderRadius: 6,
           border: "1px solid #e5e7eb",
-          boxShadow: "0px 1px 2px #1a2a4a0a",
-          overflow: "hidden",
+        }}
+      >
+        <span
+          style={{
+            fontSize: 14,
+            fontWeight: 600,
+            fontFamily: "Inter",
+            color: "#1a2a4a",
+            textAlign: "left",
+            lineHeight: "21px",
+            whiteSpace: "nowrap",
+          }}
+        >
+          Payee Information
+        </span>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          position: "relative",
         }}
       >
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            gap: 0,
-            paddingTop: 12,
-            paddingRight: 20,
-            paddingBottom: 12,
-            paddingLeft: 20,
+            gap: 6,
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingBottom: 0,
+            paddingLeft: 0,
             justifyContent: "flex-start",
             alignItems: "flex-start",
+            position: "absolute",
+            left: 24,
+            top: 20,
             width: "100%",
-            border: "1px solid #e5e7eb",
+            height: "100%",
           }}
         >
-          <span
+          <div
             style={{
-              fontSize: 14,
-              fontWeight: 600,
-              fontFamily: "Inter",
-              color: "#1a2a4a",
-              textAlign: "left",
-              lineHeight: "21px",
-              whiteSpace: "nowrap",
+              display: "flex",
+              flexDirection: "column",
+              gap: 0,
+              paddingTop: 0,
+              paddingRight: 0,
+              paddingBottom: 0,
+              paddingLeft: 0,
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              width: "100%",
             }}
           >
-            Payee Information
-          </span>
+            <span
+              style={{
+                fontSize: 12,
+                fontWeight: 500,
+                fontFamily: "Inter",
+                color: "#6b7280",
+                textAlign: "left",
+                lineHeight: "18px",
+                whiteSpace: "nowrap",
+              }}
+            >
+              Payee Account No.
+            </span>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 0,
+              paddingTop: 0,
+              paddingRight: 12,
+              paddingBottom: 0,
+              paddingLeft: 12,
+              justifyContent: "center",
+              alignItems: "flex-start",
+              width: 280,
+              height: 36,
+              backgroundColor: "#ffffff",
+              borderRadius: 4,
+              border: "1px solid #e5e7eb",
+              overflow: "hidden",
+            }}
+          >
+            <span
+              style={{
+                width: "100%",
+                fontSize: 14,
+                fontWeight: 400,
+                fontFamily: "Inter",
+                color: "#1a2a4a",
+                textAlign: "left",
+                lineHeight: "21px",
+                whiteSpace: "nowrap",
+              }}
+            >
+              6217003829165047281
+            </span>
+          </div>
         </div>
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            gap: 0,
-            paddingTop: 20,
-            paddingRight: 24,
-            paddingBottom: 20,
-            paddingLeft: 24,
+            gap: 6,
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingBottom: 0,
+            paddingLeft: 0,
             justifyContent: "flex-start",
             alignItems: "flex-start",
+            position: "absolute",
+            left: 537,
+            top: 20,
             width: "100%",
+            height: "100%",
           }}
         >
           <div
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: 6,
+              gap: 0,
               paddingTop: 0,
               paddingRight: 0,
               paddingBottom: 0,
@@ -89,77 +166,27 @@ export default function PayeeInformation() {
               justifyContent: "flex-start",
               alignItems: "flex-start",
               width: "100%",
-              height: "100%",
             }}
           >
-            <div
+            <span
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 0,
-                paddingTop: 0,
-                paddingRight: 0,
-                paddingBottom: 0,
-                paddingLeft: 0,
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-                width: "100%",
+                fontSize: 12,
+                fontWeight: 500,
+                fontFamily: "Inter",
+                color: "#6b7280",
+                textAlign: "left",
+                lineHeight: "18px",
+                whiteSpace: "nowrap",
               }}
             >
-              <span
-                style={{
-                  fontSize: 12,
-                  fontWeight: 500,
-                  fontFamily: "Inter",
-                  color: "#6b7280",
-                  textAlign: "left",
-                  lineHeight: "18px",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                Payee Account No.
-              </span>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 0,
-                paddingTop: 0,
-                paddingRight: 12,
-                paddingBottom: 0,
-                paddingLeft: 12,
-                justifyContent: "center",
-                alignItems: "flex-start",
-                width: 280,
-                height: 36,
-                backgroundColor: "#ffffff",
-                borderRadius: 4,
-                border: "1px solid #e5e7eb",
-                overflow: "hidden",
-              }}
-            >
-              <span
-                style={{
-                  width: "100%",
-                  fontSize: 14,
-                  fontWeight: 400,
-                  fontFamily: "Inter",
-                  color: "#1a2a4a",
-                  textAlign: "left",
-                  lineHeight: "21px",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                6217003829165047281
-              </span>
-            </div>
+              Account Name
+            </span>
           </div>
           <div
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: 6,
+              gap: 0,
               paddingTop: 0,
               paddingRight: 0,
               paddingBottom: 0,
@@ -167,71 +194,74 @@ export default function PayeeInformation() {
               justifyContent: "flex-start",
               alignItems: "flex-start",
               width: "100%",
-              height: "100%",
             }}
           >
-            <div
+            <span
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 0,
-                paddingTop: 0,
-                paddingRight: 0,
-                paddingBottom: 0,
-                paddingLeft: 0,
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-                width: "100%",
+                fontSize: 14,
+                fontWeight: 500,
+                fontFamily: "Inter",
+                color: "#1a2a4a",
+                textAlign: "left",
+                lineHeight: "21px",
+                whiteSpace: "nowrap",
               }}
             >
-              <span
-                style={{
-                  fontSize: 12,
-                  fontWeight: 500,
-                  fontFamily: "Inter",
-                  color: "#6b7280",
-                  textAlign: "left",
-                  lineHeight: "18px",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                Account Name
-              </span>
-            </div>
-            <div
+              Shanghai XYZ Trading Co., Ltd.
+            </span>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 6,
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingBottom: 0,
+            paddingLeft: 0,
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            position: "absolute",
+            left: 24,
+            top: 96,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 0,
+              paddingTop: 0,
+              paddingRight: 0,
+              paddingBottom: 0,
+              paddingLeft: 0,
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              width: "100%",
+            }}
+          >
+            <span
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 0,
-                paddingTop: 0,
-                paddingRight: 0,
-                paddingBottom: 0,
-                paddingLeft: 0,
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-                width: "100%",
+                fontSize: 12,
+                fontWeight: 500,
+                fontFamily: "Inter",
+                color: "#6b7280",
+                textAlign: "left",
+                lineHeight: "18px",
+                whiteSpace: "nowrap",
               }}
             >
-              <span
-                style={{
-                  fontSize: 14,
-                  fontWeight: 500,
-                  fontFamily: "Inter",
-                  color: "#1a2a4a",
-                  textAlign: "left",
-                  lineHeight: "21px",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                Shanghai XYZ Trading Co., Ltd.
-              </span>
-            </div>
+              Bank Name
+            </span>
           </div>
           <div
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: 6,
+              gap: 0,
               paddingTop: 0,
               paddingRight: 0,
               paddingBottom: 0,
@@ -239,65 +269,21 @@ export default function PayeeInformation() {
               justifyContent: "flex-start",
               alignItems: "flex-start",
               width: "100%",
-              height: "100%",
             }}
           >
-            <div
+            <span
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 0,
-                paddingTop: 0,
-                paddingRight: 0,
-                paddingBottom: 0,
-                paddingLeft: 0,
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-                width: "100%",
+                fontSize: 14,
+                fontWeight: 500,
+                fontFamily: "Inter",
+                color: "#1a2a4a",
+                textAlign: "left",
+                lineHeight: "21px",
+                whiteSpace: "nowrap",
               }}
             >
-              <span
-                style={{
-                  fontSize: 12,
-                  fontWeight: 500,
-                  fontFamily: "Inter",
-                  color: "#6b7280",
-                  textAlign: "left",
-                  lineHeight: "18px",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                Bank Name
-              </span>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 0,
-                paddingTop: 0,
-                paddingRight: 0,
-                paddingBottom: 0,
-                paddingLeft: 0,
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-                width: "100%",
-              }}
-            >
-              <span
-                style={{
-                  fontSize: 14,
-                  fontWeight: 500,
-                  fontFamily: "Inter",
-                  color: "#1a2a4a",
-                  textAlign: "left",
-                  lineHeight: "21px",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                China Construction Bank, Shanghai Branch
-              </span>
-            </div>
+              China Construction Bank, Shanghai Branch
+            </span>
           </div>
         </div>
       </div>
